84a773a38a07462c737708ffbba70c320d0b09f3
yhow11 <johnmaulion@songlyrics.com>
2016-08-02 08:32:28 +0800

    fix kafka
    fix kafka

--- sparkapp-collation-receiver/src/main/java/sparkapp/collation/receiver/Main.java
+++ sparkapp-collation-receiver/src/main/java/sparkapp/collation/receiver/Main.java
@@ -1,6 +1,5 @@
 package sparkapp.collation.receiver;
 
-
 import java.util.ArrayList;
 import kafka.utils.ZKStringSerializer$;
 import java.util.Arrays;
@@ -46,32 +45,43 @@ import usertracker.browser.model.SessionModel;
 import usertracker.browser.model.VisitorLogModel;
 import usertracker.browser.model.WebEventModel;
 import usertracker.browser.service.VisitorLogService;
+
 public class Main {
 
-	public static void main(String [] args) throws Exception {
-		
+	public static void main(String[] args) throws Exception {
+
 		Logger.getLogger("sparkapp.collation.receiver.Main").setLevel(Level.INFO);
 		Logger.getLogger("org.apache.spark.streaming.dstream.DStream").setLevel(Level.INFO);
 		Logger.getLogger("org.apache.spark.streaming.dstream.WindowedDStream").setLevel(Level.INFO);
 		Logger.getLogger("org.apache.spark.streaming.DStreamGraph").setLevel(Level.INFO);
 		Logger.getLogger("org.apache.spark.streaming.scheduler.JobGenerator").setLevel(Level.INFO);
 
-		ApplicationContext ctx = new AnnotationConfigApplicationContext(ConfigContext.class, DaoConfig.class, ServiceConfig.class, MapperConfig.class, PhoenixContext.class);
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(ConfigContext.class, DaoConfig.class,
+				ServiceConfig.class, MapperConfig.class, PhoenixContext.class);
 
 		VisitorLogService visitorLogService = (VisitorLogService) ctx.getBean("visitorLogService");
 		VisitorLogStringMapper visitorLogStringMapper = (VisitorLogStringMapper) ctx.getBean("visitorLogStringMapper");
-		
+
 		String brokers = args[0];
 		String topics = args[1];
-
-		ZkClient zkClient = new ZkClient("poc:2181", 10000, 10000, ZKStringSerializer$.MODULE$);
-		for(String topic: topics.split(",")){
-			if(!AdminUtils.topicExists(zkClient, topic)){
-				AdminUtils.createTopic(zkClient, topic, 1, 1, new Properties());
-			}
-			
-		}
 		
+		ZkClient zkClient = null;
+		try {
+			zkClient = new ZkClient("poc:2181", 10000, 10000, ZKStringSerializer$.MODULE$);
+			for (String topic : topics.split(",")) {
+				if (!AdminUtils.topicExists(zkClient, topic)) {
+					AdminUtils.createTopic(zkClient, topic, 1, 1, new Properties());
+				}
+
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			if (zkClient != null) {
+				zkClient.close();
+			}
+		}
+
 		// Create context with a 2 seconds batch interval
 		SparkConf sparkConf = new SparkConf().setAppName("CollationReceiver");
 
@@ -87,17 +97,16 @@ public class Main {
 		// Create direct kafka stream with brokers and topics
 		JavaPairInputDStream<String, String> messages = KafkaUtils.createDirectStream(jssc, String.class, String.class,
 				StringDecoder.class, StringDecoder.class, kafkaParams, topicsSet);
-		
-		if(createNewTable(args)){
+
+		if (createNewTable(args)) {
 			visitorLogService.creatTable(VisitorLogModel.class);
 			visitorLogService.creatTable(AnonymousVisitorModel.class);
 			visitorLogService.creatTable(BrowserFPModel.class);
 			visitorLogService.creatTable(DeviceFPModel.class);
 			visitorLogService.creatTable(SessionModel.class);
 			visitorLogService.creatTable(WebEventModel.class);
-			
+
 		}
-		
 
 		// Get the lines, split them into words, count the words and print
 		JavaDStream<String> lines = messages.map(line -> {
@@ -116,27 +125,28 @@ public class Main {
 				System.out.println("Saving..");
 				visitorLogModel.setId(UUID.randomUUID().toString());
 				visitorLogService.save(VisitorLogModel.class, visitorLogModel);
-				
+
 				AnonymousVisitorModel av = null;
 				BrowserFPModel browserFP = null;
 				DeviceFPModel deviceFP = null;
-				SessionModel sessionModel = visitorLogService.getOne(SessionModel.class, visitorLogModel.getSessionID());
+				SessionModel sessionModel = visitorLogService.getOne(SessionModel.class,
+						visitorLogModel.getSessionID());
 				System.out.println(sessionModel);
 				av = findAV(visitorLogService, sessionModel, visitorLogModel);
-				
-				if(sessionModel == null){
+
+				if (sessionModel == null) {
 					sessionModel = new SessionModel();
 					sessionModel.setId(visitorLogModel.getSessionID());
 					sessionModel.setAnonymousVisitorID(av.getId());
 					visitorLogService.save(SessionModel.class, sessionModel);
-					System.out.println("Created New Session "+sessionModel.getId());
+					System.out.println("Created New Session " + sessionModel.getId());
 				} else {
-					System.out.println("Session Found. "+sessionModel.getId());
+					System.out.println("Session Found. " + sessionModel.getId());
 				}
-				
+
 				browserFP = findBrowserFP(visitorLogService, av, visitorLogModel);
 				deviceFP = findDeviceFP(visitorLogService, av, visitorLogModel);
-				
+
 				WebEventModel webEvent = new WebEventModel();
 				webEvent.setId(UUID.randomUUID().toString());
 				webEvent.setAnonymousVisitorID(av.getId());
@@ -148,13 +158,12 @@ public class Main {
 				webEvent.setUrl(visitorLogModel.getUrl());
 				webEventModels.add(webEvent);
 				visitorLogService.save(WebEventModel.class, webEvent);
-				System.out.println("Created New WebEvent "+webEvent.getId());
-				
+				System.out.println("Created New WebEvent " + webEvent.getId());
+
 			}
 
-			
 			if (webEventModels.size() > 0 && broadcast(args)) {
-				try{
+				try {
 					RestTemplate rt = new RestTemplate();
 
 					rt.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
@@ -166,14 +175,14 @@ public class Main {
 					data.setType(WebEventModel.class.getSimpleName());
 					data.getData().addAll(webEventModels);
 					String result = rt.postForObject(uri, data, String.class);
-				}catch(Exception e){
+				} catch (Exception e) {
 					System.out.print(e.getMessage());
 				}
-				
+
 			}
-			
+
 			if (visitorLogModels.size() > 0 && broadcast(args)) {
-				try{
+				try {
 					RestTemplate rt = new RestTemplate();
 
 					rt.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
@@ -185,76 +194,84 @@ public class Main {
 					data.setType(VisitorLogModel.class.getSimpleName());
 					data.getData().addAll(visitorLogModels);
 					String result = rt.postForObject(uri, data, String.class);
-				}catch(Exception e){
+				} catch (Exception e) {
 					System.out.print(e.getMessage());
 				}
-				
+
 			}
 		});
 		jssc.start();
 		jssc.awaitTermination();
 	}
-	public static BrowserFPModel findBrowserFP(VisitorLogService visitorLogService, AnonymousVisitorModel av,  VisitorLogModel visitorLogModel) throws Exception{
+
+	public static BrowserFPModel findBrowserFP(VisitorLogService visitorLogService, AnonymousVisitorModel av,
+			VisitorLogModel visitorLogModel) throws Exception {
 		BrowserFPModel browserFP = visitorLogService.getOne(BrowserFPModel.class, visitorLogModel.getWebFP());
-		if(browserFP == null) {
+		if (browserFP == null) {
 			browserFP = new BrowserFPModel();
 			browserFP.setAnonymousVisitorID(av.getId());
 			browserFP.setId(visitorLogModel.getWebFP());
 			visitorLogService.save(BrowserFPModel.class, browserFP);
-			System.out.println("Created Browser FP "+browserFP.getId());
+			System.out.println("Created Browser FP " + browserFP.getId());
 		} else {
-			System.out.println("Browser FP Found. "+browserFP.getId());
+			System.out.println("Browser FP Found. " + browserFP.getId());
 		}
 		return browserFP;
 	}
-	public static DeviceFPModel findDeviceFP(VisitorLogService visitorLogService, AnonymousVisitorModel av,  VisitorLogModel visitorLogModel)  throws Exception{
+
+	public static DeviceFPModel findDeviceFP(VisitorLogService visitorLogService, AnonymousVisitorModel av,
+			VisitorLogModel visitorLogModel) throws Exception {
 		DeviceFPModel deviceFP = visitorLogService.getOne(DeviceFPModel.class, visitorLogModel.getDeviceFP());
-		if(deviceFP == null) {
+		if (deviceFP == null) {
 			deviceFP = new DeviceFPModel();
 			deviceFP.setAnonymousVisitorID(av.getId());
 			deviceFP.setId(visitorLogModel.getDeviceFP());
 			visitorLogService.save(DeviceFPModel.class, deviceFP);
-			System.out.println("Created Device FP "+deviceFP.getId());
-		}  else {
-			System.out.println("Device FP Found. "+deviceFP.getId());
+			System.out.println("Created Device FP " + deviceFP.getId());
+		} else {
+			System.out.println("Device FP Found. " + deviceFP.getId());
 		}
 		return deviceFP;
 	}
-	public static AnonymousVisitorModel findAV(VisitorLogService visitorLogService, SessionModel sessionModel,VisitorLogModel visitorLogModel) throws Exception{
+
+	public static AnonymousVisitorModel findAV(VisitorLogService visitorLogService, SessionModel sessionModel,
+			VisitorLogModel visitorLogModel) throws Exception {
 		AnonymousVisitorModel av = null;
-		if(sessionModel != null) {
+		if (sessionModel != null) {
 			System.out.println("Finding Anonymous Visitor by session..");
 			av = visitorLogService.getOne(AnonymousVisitorModel.class, sessionModel.getAnonymousVisitorID());
-			if(av != null) {
-				System.out.println("Anonymous Vistor Found. "+av.getId());
+			if (av != null) {
+				System.out.println("Anonymous Vistor Found. " + av.getId());
 				return av;
 			}
 		}
-		if(av == null) {
+		if (av == null) {
 			BrowserFPModel browserFP = visitorLogService.getOne(BrowserFPModel.class, visitorLogModel.getWebFP());
-			if(browserFP != null) {
+			if (browserFP != null) {
 				System.out.println("Finding Anonymous Visitor by Browser FP..");
 				av = visitorLogService.getOne(AnonymousVisitorModel.class, browserFP.getAnonymousVisitorID());
 			} else {
-				
+
 				av = new AnonymousVisitorModel();
 				av.setId(UUID.randomUUID().toString());
 				visitorLogService.save(AnonymousVisitorModel.class, av);
-				System.out.println("Created New Anonymous Visitor "+av.getId());
-				
+				System.out.println("Created New Anonymous Visitor " + av.getId());
+
 			}
 		}
 		return av;
 	}
-	public static boolean createNewTable(String[] args){
-		if(args.length > 2){
+
+	public static boolean createNewTable(String[] args) {
+		if (args.length > 2) {
 			return "true".equals(args[2]);
 		} else {
 			return false;
 		}
 	}
-	public static boolean broadcast(String[] args){
-		if(args.length > 3){
+
+	public static boolean broadcast(String[] args) {
+		if (args.length > 3) {
 			return "true".equals(args[3]);
 		} else {
 			return false;
