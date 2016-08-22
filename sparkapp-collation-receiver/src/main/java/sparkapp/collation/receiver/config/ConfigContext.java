27254846b116a4af790301f02912de247664e4fc
yhow11 <johnmaulion@songlyrics.com>
2016-08-23 01:56:05 +0800

    asd
    asd

--- sparkapp-collation-receiver/src/main/java/sparkapp/collation/receiver/config/ConfigContext.java
+++ sparkapp-collation-receiver/src/main/java/sparkapp/collation/receiver/config/ConfigContext.java
@@ -6,12 +6,14 @@ import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
+@PropertySource({"classpath:kafka.properties", "classpath:spark.properties"})
 public class ConfigContext {
 
-	@Value("${brokerList}")
+	@Value("${kafka.metadata.broker.list}")
 	private String brokerList;
 
 	@Bean
