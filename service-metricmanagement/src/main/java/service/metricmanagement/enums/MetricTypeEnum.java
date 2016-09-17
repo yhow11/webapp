482b673dd7ad7f4e7811bb340737fdadc0d15c17
yhow11 <johnmaulion@songlyrics.com>
2016-09-17 17:02:36 +0800

    Revert "asd"
    This reverts commit 537dc58eff117b616a6cbcbf614f27906f6884d2.

--- service-metricmanagement/src/main/java/service/metricmanagement/enums/MetricTypeEnum.java
+++ service-metricmanagement/src/main/java/service/metricmanagement/enums/MetricTypeEnum.java
@@ -11,8 +11,4 @@ public enum MetricTypeEnum {
 	MetricTypeEnum(String type){
 		this.type = type;
 	}
-	
-	public String getType() {
-		return type;
-	}
 }
--- sparkapp-collation-receiver/src/main/java/sparkapp/collation/receiver/service/impl/PartialPageCountService.java
+++ sparkapp-collation-receiver/src/main/java/sparkapp/collation/receiver/service/impl/PartialPageCountService.java
@@ -41,8 +41,8 @@ public class PartialPageCountService {
 			param.put(TABLE_PROP_KEY, MetricModel.class.getAnnotation(PhoenixTable.class).table());
 			DataFrame metricTable = sqlContext.read().format(FORMAT).options(param).load();
 			DataFrame joinTable = urlTaggedTable.join(metricTable, urlTaggedTable.col("TKEY").equalTo(metricTable.col("TKEY")));
-			joinTable = joinTable.filter(col("NAME").equalTo(MetricTypeEnum.PAGE_COUNT.getType()))
-			.select(urlTaggedTable.col("URL"), urlTaggedTable.col("TKEY"), urlTaggedTable.col("TVALUES"), metricTable.col("NAME").as("METRIC"));
+			joinTable = joinTable.filter(col("NAME").equalTo(MetricTypeEnum.PAGE_COUNT.name()))
+			.select(urlTaggedTable.col("URL"), urlTaggedTable.col("TKEY"), urlTaggedTable.col("TVALUES"), metricTable.col("NAME"));
 			joinTable.show();
 			Encoder<PartialPageCount> encoder = Encoders.bean(PartialPageCount.class);
 			Dataset<PartialPageCount> dataset = joinTable.as(encoder);
