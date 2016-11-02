7eaabd01e5b8dc203301a66519ed866581d755bb
yhow11 <johnmaulion@songlyrics.com>
2016-11-02 11:11:20 +0800

    updates

--- sparkapp-collation-receiver/src/main/java/sparkapp/collation/receiver/config/SparkConfig.java
+++ sparkapp-collation-receiver/src/main/java/sparkapp/collation/receiver/config/SparkConfig.java
@@ -22,7 +22,7 @@ public class SparkConfig {
 	@Bean
 	public SparkConf sparkConf(){
 		SparkConf config = new SparkConf().setAppName(appName);
-		config.setMaster("local");
+//		config.setMaster("local");
 		return config;
 	}
 	
